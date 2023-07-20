8c199fd43ea104d4d2d62d3a3c0167dac45b4b7a
dks1532 <127491851+dks1532@users.noreply.github.com>
2023-07-20 10:39:39 +0900

    Initial commit

---
+++ mybatis_table.sql
@@ -0,0 +1,113 @@
+--ALTER SESSION SET "_oracle_script" = true;
+--CREATE USER mybatis IDENTIFIED BY 1234;
+--GRANT CONNECT, RESOURCE TO mybatis;
+--ALTER USER mybatis DEFAULT TABLESPACE USERS QUOTA UNLIMITED ON USERS;
+-- 위 4줄은 시스템 계정에서 실행하여 권한 부여
+
+----------------------------------------------------------------
+---------------------- MEMBER TABLE --------------------------
+----------------------------------------------------------------
+CREATE TABLE MEMBER (
+    USER_ID VARCHAR2(30) PRIMARY KEY,
+    USER_PWD VARCHAR2(100) NOT NULL,
+    USER_NAME VARCHAR2(15) NOT NULL,
+    EMAIL VARCHAR2(100),
+    BIRTHDAY VARCHAR2(6),
+    GENDER VARCHAR2(1) CHECK (GENDER IN('M', 'F')),
+    PHONE VARCHAR2(13),
+    ADDRESS VARCHAR2(100),
+    ENROLL_DATE DATE DEFAULT SYSDATE,
+    MODIFY_DATE DATE DEFAULT SYSDATE,
+    STATUS VARCHAR2(1) DEFAULT 'Y' CHECK(STATUS IN('Y', 'N'))
+);
+
+INSERT INTO MEMBER
+VALUES ('admin', '1234', '관리자', 'admin@kh.or.kr', '800918', 'F', '010-1111-2222','서울시 영등포구 당산동', '20221211', DEFAULT, DEFAULT);
+
+INSERT INTO MEMBER
+VALUES ('user01', '1234', '홍길동', 'user01@kh.or.kr', '900213', 'M','010-3333-4444','서울시 양천구 목동','20221128',DEFAULT, DEFAULT);
+
+----------------------------------------------------------------
+------------------------ BOARD TABLE ---------------------------
+----------------------------------------------------------------
+CREATE TABLE BOARD(
+    BOARD_NO NUMBER PRIMARY KEY,
+    BOARD_TITLE VARCHAR2(100) NOT NULL,
+    BOARD_CONTENT VARCHAR2(4000) NOT NULL,
+    BOARD_WRITER VARCHAR2(30),
+    COUNT NUMBER DEFAULT 0,
+    CREATE_DATE DATE DEFAULT SYSDATE,
+    STATUS VARCHAR2(1) DEFAULT 'Y' CHECK (STATUS IN('Y', 'N')),
+    FOREIGN KEY (BOARD_WRITER) REFERENCES MEMBER
+);
+
+CREATE SEQUENCE SEQ_BNO NOCACHE;
+
+INSERT INTO BOARD
+VALUES(SEQ_BNO.NEXTVAL, '첫번째 게시판 서비스를 시작하겠습니다.', '안녕하세요. 첫 게시판입니다.','admin', DEFAULT, '20220311', DEFAULT);
+
+INSERT INTO BOARD
+VALUES(SEQ_BNO.NEXTVAL, '두번째 게시판 서비스를 시작하겠습니다.', '안녕하세요. 2 게시판입니다.','user01', DEFAULT, '20220315', DEFAULT);
+
+INSERT INTO BOARD
+VALUES(SEQ_BNO.NEXTVAL, '하이 에브리원 게시판 서비스를 시작하겠습니다.', '안녕하세요. 3 게시판입니다','user01',DEFAULT, '20220319', DEFAULT);
+
+INSERT INTO BOARD
+VALUES(SEQ_BNO.NEXTVAL, '안녕.. 마이바티스는 처음이지?', '안녕하세요. 첫 게시판입니다.', 'user01',DEFAULT, '20220320', DEFAULT);
+
+INSERT INTO BOARD
+VALUES(SEQ_BNO.NEXTVAL, '안녕. 제목1', '안녕 내용1', 'admin', DEFAULT, '20220323', DEFAULT);
+
+INSERT INTO BOARD
+VALUES(SEQ_BNO.NEXTVAL, '페이징 처리때문에 샘플데이터 많이 넣어놓는다...', '안녕하십니까', 'admin',DEFAULT,  '20220324', DEFAULT);
+
+INSERT INTO BOARD
+VALUES(SEQ_BNO.NEXTVAL,'제목2' ,'내용2', 'admin', DEFAULT, '20220325',DEFAULT);
+
+INSERT INTO BOARD
+VALUES(SEQ_BNO.NEXTVAL,'제목3' ,'내용3' , 'admin', DEFAULT, '20220326', DEFAULT);
+
+INSERT INTO BOARD
+VALUES(SEQ_BNO.NEXTVAL,'제목4' ,'내용4', 'user01', DEFAULT, '20220327', DEFAULT);
+
+INSERT INTO BOARD
+VALUES(SEQ_BNO.NEXTVAL, '제목5' ,'내용5', 'admin', DEFAULT, '20220328', DEFAULT);
+
+INSERT INTO BOARD
+VALUES(SEQ_BNO.NEXTVAL,'제목6' ,'내용6', 'user01', DEFAULT, '20220329', DEFAULT);
+
+INSERT INTO BOARD
+VALUES(SEQ_BNO.NEXTVAL, '제목7' ,'내용7', 'admin', DEFAULT, '20220401', DEFAULT);
+
+INSERT INTO BOARD
+VALUES(SEQ_BNO.NEXTVAL, '마지막 게시판 시작하겠습니다.', '안녕하세요. 마지막 게시판입니다.', 'admin',  DEFAULT, '20220403', DEFAULT);
+
+----------------------------------------------------------------
+------------------------ REPLY TABLE ---------------------------
+----------------------------------------------------------------
+CREATE TABLE REPLY(
+    REPLY_NO NUMBER PRIMARY KEY,
+    REPLY_CONTENT VARCHAR2(400),
+    REF_BNO NUMBER,
+    REPLY_WRITER VARCHAR2(30),
+    CREATE_DATE DATE DEFAULT SYSDATE,
+    STATUS VARCHAR2(1) DEFAULT 'Y' CHECK (STATUS IN ('Y', 'N')),
+    FOREIGN KEY (REF_BNO) REFERENCES BOARD,
+    FOREIGN KEY (REPLY_WRITER) REFERENCES MEMBER
+);
+
+CREATE SEQUENCE SEQ_RNO NOCACHE;
+
+INSERT INTO REPLY
+VALUES(SEQ_RNO.NEXTVAL,'첫번째 댓글입니다', 1, 'user01', '20201225', DEFAULT);
+
+INSERT INTO REPLY
+VALUES(SEQ_RNO.NEXTVAL, '첫번째 댓글입니다', 13, 'user01', '20220412', DEFAULT);
+
+INSERT INTO REPLY
+VALUES(SEQ_RNO.NEXTVAL, '두번째 댓글입니다', 13, 'user01', '20220413', DEFAULT);
+
+INSERT INTO REPLY
+VALUES(SEQ_RNO.NEXTVAL, '마지막 댓글입니다', 13, 'user01', '20220414', DEFAULT);
+
+commit;
